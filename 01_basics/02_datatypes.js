e2a29bf91e62a11d1898237027849c1c136767b2
heyDev07 <130311688+heyDev07@users.noreply.github.com>
2024-05-15 11:42:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //to use our code as a newer version of js
+// alert(3+3) this syntex if for web browser not in node node has a different syntex
+//number 
+//bigint
+//string => " " or ' '
+//boolean =?yes or no
+//null =>stand alone value
+//undefined => value is not assigned
+//symbol =>use for uniquenesss
+//object=>
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,8 @@
+const ID =1243;
+// ID=2
+let nama="hey";
+let pass="123451"
+let cityname="dholpur"
+let statename="rajasthan"
+// var is not use because of scope problem { } it has no scope so can be changed by others too
+console.table([ID , nama ,pass ,cityname ,statename])
